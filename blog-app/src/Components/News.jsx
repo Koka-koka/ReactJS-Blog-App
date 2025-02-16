13dd020f107f77903830a49e3a74c05340a22d49
Koka-koka <kokamerabishvili@gmail.com>
2025-02-17 03:07:57 +0400

    Create app layout.

--- blog-app/src/Components/News.jsx
+++ blog-app/src/Components/News.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Weather from "./Weather";
+import Calendar from "./Calendar";
+import "./News.css";
 
 const News = () => {
   return (
-    <div>News</div>
-  )
-}
+    <div className="news">
+      <header className="news__header">News Header</header>
+      <div className="news__content">
+        <div className="navbar">
+          <div className="user">User</div>
+          <nav className="categories">Categories</nav>
+        </div>
+        <div className="news__section">
+          <div className="headline">Headline</div>
+          <div className="news__grid">News Grid</div>
+        </div>
+        <div className="my-blogs">My Blogs</div>
+        <div className="weather-calendar">
+          <Weather />
+          <Calendar />
+        </div>
+      </div>
+      <footer className="news__footer">Footer</footer>
+    </div>
+  );
+};
 
-export default News
+export default News;
